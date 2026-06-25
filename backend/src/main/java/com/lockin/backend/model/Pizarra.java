11921d1249802cdc4ebf30ffd404dd471a49927c
javigm8 <javigarcia4848@gmail.com>
2026-06-26 01:11:18 +0200

    Cambio tipo datos en modelo Pizarra

--- backend/src/main/java/com/lockin/backend/model/Pizarra.java
+++ backend/src/main/java/com/lockin/backend/model/Pizarra.java
@@ -1,8 +1,16 @@
 package com.lockin.backend.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "pizarra")
@@ -16,7 +24,7 @@ public class Pizarra {
     @Column(nullable = false)
     private String titulo;
 
-    @Column(columnDefinition = "json")
+    @Column(columnDefinition = "text")
     private String datos;
 
     @Column(nullable = false)
