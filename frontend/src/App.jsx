1c3f11794acce6819e3a8c88a21ce1210f3cb8df
javigm8 <javigarcia4848@gmail.com>
2026-06-27 18:02:23 +0200

    Módulos añadidos en sus versiones de páginas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,44 +4,29 @@ import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import Settings from './pages/Settings'
 import Whiteboard from './pages/Whiteboard'
+import TasksPage from './pages/TasksPage'
+import NotesPage from './pages/NotesPage'
+import TempPage from './pages/TempPage'
+import StatisticsPage from './pages/StatisticsPage'
+import AppLayout from './AppLayout'
 
 function App() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={<Login />}
-      />
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/pizarra" element={<Whiteboard />} />
 
-      <Route
-        path="/register"
-        element={<Register />}
-      />
+      <Route element={<AppLayout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/tareas" element={<TasksPage />} />
+        <Route path="/notas" element={<NotesPage />} />
+        <Route path="/temporizador" element={<TempPage />} />
+        <Route path="/estadisticas" element={<StatisticsPage />} />
+        <Route path="/settings" element={<Settings />} />
+      </Route>
 
-      <Route
-        path="/dashboard"
-        element={<Dashboard />}
-      />
-
-      <Route
-        path="/settings"
-        element={<Settings />}
-      />
-
-      <Route
-        path="/pizarra"
-        element={<Whiteboard />}
-      />
-
-      <Route
-        path="*"
-        element={
-          <Navigate
-            to="/"
-            replace
-          />
-        }
-      />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
 }
